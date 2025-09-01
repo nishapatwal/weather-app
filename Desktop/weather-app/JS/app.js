51e99df9f615dced9d7b39f92980a83a1a525c17
NISHA PATWAL <nisha.patwal2006@gmail.com>
2025-09-02 02:45:10 +0530

    Added input field for entering city nam and basic JavaScript handling

---
+++ Desktop/weather-app/JS/app.js
@@ -0,0 +1,16 @@
+// write the program to print the marks of a student in an object using for loop { KARAN:98 ; ROHAN:70 ;AYUSH:07}
+/*let statement = {
+    KARAN: 98,
+    ROHAN: 70,
+    AYUSH: 7
+};
+console.log("Student marks:");
+for (let key in statement) {
+    console.log(key + " : " + statement[key]);
+}*/
+document.getElementById('search-btn').addEventListener('click',
+    function() {
+    const city = document.getElementById('city-input').value;
+    console.log('City entered:', city);
+    // Additional code to fetch weather data will be added later
+    });
